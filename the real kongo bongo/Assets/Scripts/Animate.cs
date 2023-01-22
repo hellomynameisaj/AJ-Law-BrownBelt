5102df8201b3c8bfcf04796b2040a5bfd00453ad
Aroosh <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-22 14:59:24 -0500

    e
    e

---
+++ the real kongo bongo/Assets/Scripts/Animate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+
+    Animator Animator;
+
+    Jump Jump;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
