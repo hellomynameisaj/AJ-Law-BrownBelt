c9d6fc196273b8f516e899e1ca7e604de21f0b74
alisqon <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-28 15:59:13 -0500

    a
    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ kastle kaos/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, Vector3.forward, .15f);
+        Debug.DrawRay(transform.position, Vector3.forward * .15f, Color.red);
+    }
+}
