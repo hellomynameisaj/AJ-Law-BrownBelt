d70bfe125fd30f41659efdac1efdc0c11928b0ee
alisqon <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-28 14:42:12 -0500

    dare
    to

--- the real kongo bongo/Assets/Scripts/Animate.cs
+++ the real kongo bongo/Assets/Scripts/Animate.cs
@@ -12,11 +12,39 @@ public class Animate : MonoBehaviour
     void Start()
     {
         Animator = GetComponent<Animator>();
+        Jump = GetComponent<Jump>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Jump.isGrounded)
+        {
+            Animator.SetBool("isJumping", true);
+            Animator.SetBool("isIdle", false);
+            Animator.SetBool("isWalking", false);
+            Animator.SetBool("isWalkingBackwards", false);
+        }
+
+        if (Jump.isGrounded)
+        {
+            Animator.SetBool("isJumping", false);
+            Animator.SetBool("isIdle", true);
+            Animator.SetBool("isWalking", false);
+            Animator.SetBool("isWalkingBackwards", false);
+
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                Animator.SetBool("isIdle", false);
+                Animator.SetBool("isWalking", true);
+                Animator.SetBool("isWalkingBackwards", false);
+            }
+            if(Input.GetAxisRaw("Vertical") == -1)
+            {
+                Animator.SetBool("isIdle", false);
+                Animator.SetBool("isWalking", false);
+                Animator.SetBool("isWalkingBackwards", true);
+            }
+        }
     }
 }
