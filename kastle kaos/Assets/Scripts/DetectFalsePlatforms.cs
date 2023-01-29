0b29e54dcf52261ed32f291c68189d76db73dec5
Cassidy <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-29 14:07:01 -0500

    guys
    that is just a duck

--- kastle kaos/Assets/Scripts/DetectFalsePlatforms.cs
+++ kastle kaos/Assets/Scripts/DetectFalsePlatforms.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class DetectFalsePlatforms : MonoBehaviour
 {
     public bool hit;
+
+    int layerMask = 1 << 8;
     void Update()
     {
-        hit = Physics.Raycast(transform.position, Vector3.forward, .15f);
-        Debug.DrawRay(transform.position, Vector3.forward * .15f, Color.red);
+        hit = Physics.Raycast(transform.position, transform.forward, 2f, layerMask);
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.red);
+
+        if(hit == true)
+        {
+            Debug.Log("FU-");
+        } 
+        else
+        {
+            Debug.Log("go now");
+        }
     }
 }
