49c7657f3010af40275395652e59cf9f2ce22d7c
proper name <102189845+hellomynameisaj@users.noreply.github.com>
2023-03-18 15:58:04 -0400

    black and white
    there
    was
    never

---
+++ codey and the forgotten land/Assets/Scripts/LifeHUD.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeHUD : MonoBehaviour
+{
+    private GameObject[] hearts;
+    private int lives = 3;
+    public GameObject background;
+    public GameObject HealthBar;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        hearts = GameObject.FindGameObjectsWithTag("heart");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtPlayer()
+    {
+        Debug.Log("yeow im going to die ");
+        lives -= 1;
+        hearts[lives].SetActive(false);
+        if(lives == 0)
+        {
+            background.GetComponent<GameManager>().GameOver();
+        }
+    }
+}
