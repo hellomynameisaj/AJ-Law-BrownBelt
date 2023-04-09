f36707e4503fff31bc3dfc9b0e6ea08b4ac06ddc
I'mDivorcedand50 <102189845+hellomynameisaj@users.noreply.github.com>
2023-04-09 14:38:36 -0400

    picture your eating ice cream on a sunny day
    but you're divorced and 50

---
+++ ass/Assets/Scripts/DialogOpen.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogOpen : MonoBehaviour
+{
+
+    public string dialog;
+    public GameObject interfaceManager;
+    public PlayerHolding pHolding;
+    public bool begin = true;
+    public bool end = false;
+    private string[] collectibles;
+    private int clue;
+
+    private AudioSource greeting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greeting = GetComponent<AudioSource>();
+        collectibles = new string[] { "film", "balloons", "life saver", "bull's eye", "pipe", "key", "fish", "birdhouse", "red airhorn", "magic hat" };
+        createClue();
+    }
+
+    public void createClue()
+    {
+        clue = Random.Range(0, 9);
+        searchDialog();
+    }
+
+    public void searchDialog()
+    {
+        dialog = "find my " + collectibles[clue] + " and i will give you an amazing cash prize of 1 cent.";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!begin && pHolding.Verify())
+        {
+            checkClue();
+        }
+        greeting.Play(0);
+        interfaceManager.GetComponent<InterfaceManager>().ShowBox(dialog, clue);
+    }
+
+    private void checkClue()
+    {
+        if (pHolding.holdValue == clue)
+        {
+            dialog = "thanks for the  " + collectibles[clue] + " i'll give you the cent later";
+            end = true;
+        }
+        else
+        {
+            dialog = "i want my " + collectibles[clue] + " not whatever this is";
+        }
+    }
+
+    public void coinsScattered()
+    {
+        begin = false;
+    }
+
+}
