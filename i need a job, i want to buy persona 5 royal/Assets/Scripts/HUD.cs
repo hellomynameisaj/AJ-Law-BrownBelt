e765c823c5ec3ff0c10ca17cdb67d13109c098f0
Aroosh <102189845+hellomynameisaj@users.noreply.github.com>
2023-04-16 14:57:58 -0400

    there are wonders
    that are waiting

---
+++ i need a job, i want to buy persona 5 royal/Assets/Scripts/HUD.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+    public Text remainingText;
+    public Text remainingSubtext;
+    public Text targetText;
+    public Text targetSubtext;
+    public Text scoreText;
+    
+    public Image[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+
+    private void Start()
+    {
+        UpdateStars();
+    }
+
+    public void UpdateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if(i == starIndex)
+            {
+                stars[i].enabled = true;
+            } else
+            {
+                stars[i].enabled = false;
+            }
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+
+        int visibleStar = 0;
+
+        if(score >= level.score1Star && score < level.score2Star)
+        {
+            visibleStar = 1;
+        }
+        else if (score >= level.score2Star && score < level.score3Star)
+        {
+            visibleStar = 2;
+        }
+        else if (score >= level.score3Star)
+        {
+            visibleStar = 3;
+        }
+
+        starIndex = visibleStar;
+
+        UpdateStars();
+    }
+
+    public void SetTarget(int target)
+    {
+        targetText.text = target.ToString();
+    }
+
+    public void SetRemaining(int remaining)
+    {
+        remainingText.text = remaining.ToString();
+    }
+
+    public void SetRemaining(string remaining)
+    {
+        remainingText.text = remaining;
+    }
+
+    public void SetLevelType(Level.LevelType type)
+    {
+        switch (type)
+        {
+            case Level.LevelType.MOVES:
+                remainingSubtext.text = "Moves Remaining";
+                targetSubtext.text = "Target Score";
+                break;
+            case Level.LevelType.OBSTACLE:
+                remainingSubtext.text = "Moves Remaining";
+                targetSubtext.text = "Dishes Remaining";
+                break;
+            case Level.LevelType.TIMER:
+                remainingSubtext.text = "Time Remaining";
+                targetSubtext.text = "Target Score";
+                break;
+        }
+    }
+
+    public void OnGameWin(int score)
+    {
+        isGameOver = true;
+    }
+
+    public void OnGameLose()
+    {
+        isGameOver = false;
+    }
+}
---
+++ i need a job, i want to buy persona 5 royal/Assets/Scripts/LevelMoves.cs
@@ -0,0 +1,63 @@
+﻿public class LevelMoves : Level
+{
+    public int numMoves;
+    public int targetScore;
+
+    private int movesUsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = LevelType.MOVES;
+
+        for(int i=0; i<obstacleTypes.Length; i++)
+        {
+            numObstaclesLeft += grid.GetPiecesOfType(obstaclesTypes[i]).Count;
+        }
+
+        hud.SetLevelType(type);
+        hud.SetScore(currentScore);
+        hud.SetTarget(targetScore);
+        hud.SetRemaining(numMoves);
+    }
+
+    public override void OnMove()
+    {
+        base.OnMove();
+
+        movesUsed++;
+
+        hud.SetRemaining(numMoves = movesUsed);
+
+        if(numMoves - movesUsed == 0)
+        {
+            if(currentScore >= targetScore)
+            {
+                GameWin();
+            }
+            else
+            {
+                GameLose();
+            }
+        }
+    }
+
+    public override void OnPieceCleared(GamePiece piece)
+    {
+        base.OnPieceCleared(piece);
+
+        for (int i = 0; i < obstacleTypes.Length; i++)
+        {
+            numObstaclesLeft--;
+
+            hud.SetTarget(numObstaclesLeft);
+
+            if(numObstaclesLeft == 0)
+            {
+                currentScore += 1000 * (numMoves - movesUsed);
+                hud.SetScore(currentScore);
+                GameWin();
+            }
+        }
+    }
+}
