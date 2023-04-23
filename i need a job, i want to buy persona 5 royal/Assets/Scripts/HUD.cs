cdcb70a2d8d652ab0e37dbefdc7fdae25ecf578d
no <102189845+hellomynameisaj@users.noreply.github.com>
2023-04-23 15:02:09 -0400

    one of the final commits
    before i move on to red belt

--- i need a job, i want to buy persona 5 royal/Assets/Scripts/HUD.cs
+++ i need a job, i want to buy persona 5 royal/Assets/Scripts/HUD.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
     public Level level;
+    public GameOver gameOver;
+
     public Text remainingText;
     public Text remainingSubtext;
     public Text targetText;
@@ -96,11 +99,16 @@ public class HUD : MonoBehaviour
 
     public void OnGameWin(int score)
     {
-        isGameOver = true;
+        gameOver.ShowWin(score, starIndex);
+
+        if (starIndex > PlayerPrefs.GetInt(SceneManager.GetActiveScene().name, 0))
+        {
+            PlayerPrefs.SetInt(SceneManager.GetActiveScene().name, starIndex);
+        }
     }
 
     public void OnGameLose()
     {
-        isGameOver = false;
+        gameOver.ShowLose();
     }
 }
---
+++ i need a job, i want to buy persona 5 royal/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+
+    private void Start()
+    {
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            int score = PlayerPrefs.GetInt(buttons[i].playerPrefKey, 0);
+
+            for (int starIndex = 1; starIndex <=3; starIndex++)
+            {
+                Transform star = buttons[i].gameObject.transform.Find("star" + starIndex);
+                if(starIndex <= score)
+                {
+                    star.gameObject.SetActive(true);
+                }
+                else
+                {
+                    star.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+    public void OnButtonPress(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void OnDoneClicked()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    [System.Serializable]
+    public struct ButtonPlayerPrefs
+    {
+        public GameObject gameObject;
+        public string playerPrefKey;
+    }
+
+    public ButtonPlayerPrefs[] buttons;
+}
